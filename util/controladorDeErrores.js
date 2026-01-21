0a22d5f688a36c6b4ae16a388ab22549025f5565
DiriARG <matidirisio@gmail.com>
2026-01-21 14:53:41 -0300

    Mejora del controlador de errores: Errores de duplicados.

--- util/controladorDeErrores.js
+++ util/controladorDeErrores.js
@@ -1,11 +1,14 @@
 const controladorDeErrores = (error, request, response, next) => {
   console.error(error.message);
 
+  // Errores de validación (ej: campo vacío).
   if (error.name === "SequelizeValidationError") {
     return response.status(400).json({ error: error.message });
-  }
-
-  if (error.name === "SequelizeDatabaseError") {
+  } 
+  // Errores de duplicados.
+  else if (error.name === "SequelizeUniqueConstraintError") {
+    return response.status(400).json({ error: "El nombre de usuario ya existe" });
+  } else if (error.name === "SequelizeDatabaseError") {
     return response.status(400).json({ error: "Error de base de datos" });
   }
 
